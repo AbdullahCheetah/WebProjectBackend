5e74da6569c1bad0ab831e34a3d61d7e92cb4ba4
codesbyUmair <i211182@nu.edu.pk>
2023-12-19 15:20:52 +0500

    show, edit and delete doctors

--- routes/RecipientRoute.js
+++ routes/RecipientRoute.js
@@ -1,7 +1,10 @@
-const {addDoctor} = require('../controllers/RecipientController');
+const { addDoctor, updateDoctor, deleteDoctor, getAllDoctor } = require('../controllers/RecipientController');
 const express = require('express');
 const router = express.Router();
 
 router.post("/add", addDoctor);
+router.put("/update/:id", updateDoctor);
+router.delete("/delete/:id", deleteDoctor);
+router.get("/getAll", getAllDoctor);
 
 module.exports = router;
